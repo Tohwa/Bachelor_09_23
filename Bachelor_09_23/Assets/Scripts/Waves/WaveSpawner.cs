21b63b104bec297bdf8419f6a4b7b55ec7dd9ee3
Sascha <Tohwa.gaming@gmail.com>
2023-10-09 05:48:59 +0200

    added WaveManager

---
+++ Bachelor_09_23/Assets/Scripts/Waves/WaveSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField] private Waves[] waves;
+}
+
+[System.Serializable]
+public class Waves
+{
+    [SerializeField] private WaveSettings[] waveSettings;
+}
+
+[System.Serializable]
+public class WaveSettings
+{
+    #region Fields
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject spawnLocation;
+    //[SerializeField] private float spawnDelay;
+    #endregion
+}
